0f3ba0aa2c453636280cc744e097b9c6e527f0aa
Adam Salt <salt48186@gmail.com>
2023-03-27 10:43:30 -0700

    first commit

---
+++ src/test/java/TestDemoTest.java
@@ -0,0 +1,57 @@
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class TestDemoTest {
+	
+private TestDemo testDemo;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		testDemo = new TestDemo();
+	}
+
+	@ParameterizedTest
+	@MethodSource("TestDemoTest#argumentsForAddPositive")
+	void assertThatTwoPositiveNumbersAreAddedCorrectly(int a, int b, int expected, boolean expectException) {
+		if (!expectException) {
+			assertThat(testDemo.addPositive(a, b)).isEqualTo(expected);
+		} else {
+			assertThatThrownBy(() -> testDemo.addPositive(a, b)).isInstanceOf(IllegalArgumentException.class).hasMessage("Both parameters must be positive!");
+		}
+	}
+
+	
+	static Stream<Arguments> argumentsForAddPositive() {
+		return Stream.of(
+				arguments(2, 4, 6, false),
+				arguments(0, 0, 0, true),
+				arguments(1, -1, 0, true),
+				arguments(Integer.MAX_VALUE, 1, Integer.MIN_VALUE, false),
+				arguments(Integer.MIN_VALUE, -1, Integer.MAX_VALUE, true)
+				);
+	}
+	@Test
+	void assertThatNumberedSquaredIsCorrect() {
+		TestDemo mockDemo = spy(new TestDemo());
+		doReturn(5).when(mockDemo).getRandomInt();
+		
+		int actual = mockDemo.randomNumberedSquared();
+		int expected = 25;
+		
+		assertThat(actual).isEqualTo(expected);
+	}
+	
+	
+}
